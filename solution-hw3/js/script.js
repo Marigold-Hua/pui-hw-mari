c2e53a026b66950c12308df10b4f53ecc366eaf1
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2024-02-11 16:46:24 -0500

    Notes to self + selecting glazing change + reflecting change in price

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -17,6 +17,17 @@ function glazingChange(element) {
     // code to update price
 }
  */
-
+//Select priceCalculated element - might want to use getElementByID instead?
 let priceCalc = document.querySelector(".priceCalculated");
-console.log(priceCalc);
+//Seperate by each element that's changing? in my mind p much same logic for glazing and pack size 
+//Then result of logic for two streams is combined for final price changes
+let glazingSelected = priceCalc.value;
+//Check that correct element is selected: console.log(priceCalc);
+//Trying to get what is selected
+
+priceCalc.onChange = changedSelection(); 
+
+function changedSelection() {
+    var glazingSelected = priceCalc.value;
+    var text = priceCalc[else.selectedIndex].text;
+}
