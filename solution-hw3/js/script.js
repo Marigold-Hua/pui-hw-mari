11cf8b3d07fb7283134bc6fc21ff393ab314eecf
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2024-02-11 16:27:20 -0500

    Added hyperlink to logo

---
+++ solution-hw3/js/script.js
@@ -0,0 +1,9 @@
+//Glazing option logic 
+const glazingSelected = document.getElementByID("glazingOptions");
+console.log(glazingSelected);
+
+function glazingChange(element) {
+    // get value of selected glazing option
+    // const priceChange = Product;
+    // code to update price
+}
