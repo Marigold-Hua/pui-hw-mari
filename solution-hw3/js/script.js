419e5e132590cfdd71e92270eb1d18dcc81742a2
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2024-02-11 16:32:20 -0500

    Add hyperlink to logo + selector

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,4 +1,13 @@
-//Glazing option logic 
+//context - use .onchange event to listen for when select value changes
+//question - can you walk through the two steps - specifically what event listener and event handler are and how they work together?
+//Glazing option event handler
+/*
+function onGlazingUpdate() {
+    alert("Glazing updated!");
+}
+
+document.querySelector("#glazingOptions").addEventListener("click", onUpdateClick)
+
 const glazingSelected = document.getElementByID("glazingOptions");
 console.log(glazingSelected);
 
@@ -7,3 +16,6 @@ function glazingChange(element) {
     // const priceChange = Product;
     // code to update price
 }
+ */
+
+let priceCalc = document.querySelector("")
