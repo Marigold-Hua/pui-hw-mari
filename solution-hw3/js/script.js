5282785804e8150c90e5388b5a6091a038331ef5
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2024-02-11 22:40:51 -0500

    Created price change logic
    Note to check if priceEffect changes globally based on function changes

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,33 +1,33 @@
-//context - use .onchange event to listen for when select value changes
-//question - can you walk through the two steps - specifically what event listener and event handler are and how they work together?
-//Glazing option event handler
-/*
-function onGlazingUpdate() {
-    alert("Glazing updated!");
-}
+const order = ["Sugar milk", 12]
+const priceEffect = [];
+//need to check if changePriceEffect updates global priceEffect -->
+// might need to do priceEffect = this.priceEffect
+console.log(changePriceEffect(order))
 
-document.querySelector("#glazingOptions").addEventListener("click", onUpdateClick)
-
-const glazingSelected = document.getElementByID("glazingOptions");
-console.log(glazingSelected);
-
-function glazingChange(element) {
-    // get value of selected glazing option
-    // const priceChange = Product;
-    // code to update price
-}
- */
-//Select priceCalculated element - might want to use getElementByID instead?
-let priceCalc = document.querySelector(".priceCalculated");
-//Seperate by each element that's changing? in my mind p much same logic for glazing and pack size 
-//Then result of logic for two streams is combined for final price changes
-let glazingSelected = priceCalc.value;
-//Check that correct element is selected: console.log(priceCalc);
-//Trying to get what is selected
-
-priceCalc.onChange = changedSelection(); 
-
-function changedSelection() {
-    var glazingSelected = priceCalc.value;
-    var text = priceCalc[else.selectedIndex].text;
+function changePriceEffect  (orderDetails){
+    //Matches selected glazing to price effect
+    if (orderDetails[0]=== "Vanilla milk") {
+        priceEffect.push(0.5);
+    }
+    else if(orderDetails[0]=== "Double chocolate") {
+        priceEffect.push(1.5);
+    }
+    else {
+        priceEffect.push(0);
+    }
+        
+    if (orderDetails[1]=== 1) {
+        priceEffect.push(1);
+    }
+    else if(orderDetails[1]=== 3) {
+        priceEffect.push(3);
+    }
+    else if(orderDetails[1]=== 6) {
+        priceEffect.push(5);
+    }
+    else {
+        priceEffect.push(10);
+    }
+    
+    return priceEffect;
 }
