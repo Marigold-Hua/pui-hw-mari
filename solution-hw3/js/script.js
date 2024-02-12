ab098f70a2b6e0ef9a6cb935c361d0c884c27d1c
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2024-02-12 18:42:55 -0500

    Add event listener for select change

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,10 +1,15 @@
-const order = ["Sugar milk", 12]
+/** const basePrice = 2.49;
+const order = ["Double chocolate", 1]
 const priceEffect = [];
 //need to check if changePriceEffect updates global priceEffect -->
 // might need to do priceEffect = this.priceEffect
-console.log(changePriceEffect(order))
+console.log(updateCost(changePriceEffect(order)));
 
-function changePriceEffect  (orderDetails){
+//NEED to empty array
+document.getElementById("glazingOptions").addEventListener("click", displayDate);
+
+element
+function changePriceEffect(orderDetails){
     //Matches selected glazing to price effect
     if (orderDetails[0]=== "Vanilla milk") {
         priceEffect.push(0.5);
@@ -31,3 +36,78 @@ function changePriceEffect  (orderDetails){
     
     return priceEffect;
 }
+
+function updateCost(costParameters){
+    return (basePrice + costParameters[0])*costParameters[1];
+}
+
+const order = 
+{
+    orderGlaze: "This is the glazing of the order",
+    orderSize: "This is the pack size of the order"
+};
+
+
+**/
+let glazeType = "";
+let packSize = "";
+let glazePremium = "";
+let packSizeMultiplier = "";
+
+function updateOrder() {
+    glazeType = document.getElementById("glazingOptions").value;
+    packSize = document.getElementById("packSize").value;
+    console.log(glazeType, " ", packSize);
+}
+
+function matchPriceEffect() {
+
+}
+
+document.getElementById("glazingOptions").addEventListener('change', updateOrder);
+document.getElementById("packSize").addEventListener('change', updateOrder);
+/** 
+ * 
+let glazeType = "";
+let packSize = "";
+let glazePremium = "";
+let packSizeMultiplier = "";
+
+function updateOrder() {
+    glazeType = document.getElementById("glazingOptions").value;
+    packSize = document.getElementById("packSize").value;
+    matchPriceEffect(glazeType, packSize);
+}
+
+
+function matchPriceEffect(glazeType, packSize) {
+    if (glazeType === "Vanilla milk") {
+        glazePremium = 0.5;
+    }
+    else if(glazeType === "Double chocolate") {
+        glazePremium = 1.5;
+    }
+    else {
+        glazePremium = 0;
+    }
+        
+    if (packSize === 1) {
+        packSizeMultiplier = 1;
+    }
+    else if(packSize === 3) {
+        packSizeMultiplier = 3;
+    }
+    else if(packSize === 6) {
+        packSizeMultiplier = 5;
+    }
+    else {
+        packSizeMultiplier = 10;
+    }
+
+    console.log(glazeType, glazePremium, packSize, packSizeMultiplier);
+    
+}
+
+document.getElementById("glazingOptions").addEventListener('change', updateOrder());
+document.getElementById("packSize").addEventListener('change', updateOrder()); 
+**/
