5bcbcfa8315d4742038fec9a58fe9b574431a1db
Marigold-Hua <157081300+Marigold-Hua@users.noreply.github.com>
2024-02-13 09:30:41 -0500

    Updating total price and reducing code redundancy
    Wahoooooo!!!!!!

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,9 +1,9 @@
 //Key:Value pairs matching glazing and size with appropriate premiums and price multipliers
 const glazeList = {
-    "Keep original": 0,
-    "Sugar milk": 0,
-    "Vanilla milk": 0.5,
-    "Double chocolate": 1.5
+    "Keep original": 0.0,
+    "Sugar milk": 0.0,
+    "Vanilla milk":  0.5,
+    "Double chocolate":  1.5
 };
 
 const sizeList = {
@@ -16,48 +16,26 @@ const sizeList = {
 //The base price of a cinnamon roll
 const basePrice = 2.49;
 
-let glazeType = "";
-let packSize = "";
-let glazePremium = "";
-let packSizeMultiplier = "";
-
-//Populate select options for glazes and sizes
-populateGlazes();
-populateSizes();
-document.getElementById("glazingOptions").addEventListener('change', updateOrder);
-document.getElementById("packSize").addEventListener('change', updateOrder);
-
-//Use loop to populate glaze select options
-function populateGlazes() {
-    const glazes = document.getElementById("glazingOptions");
-    
-    //Clear options
-    glazes.innerHTML= " ";
-    
-    for (var key in glazeList) {
-        var option = document.createElement("option")
-        option.text = key;
-        option.value = glazeList[key];
-        glazes.appendChild(option);
-    }
+//Populate select options for glazes 
+for (var key in glazeList) {
+    var option = document.createElement("option")
+    option.text = key;
+    option.value = glazeList[key];
+    document.getElementById("glazingOptions").appendChild(option);
 }
 
-//Use loop to populate size select options
-function populateSizes() {
-   const sizes = document.getElementById("packSize");
-    
-    //Clear options
-    sizes.innerHTML= " ";
-    
-    for (var key in sizeList) {
-        var option = document.createElement("option")
-        option.text = key;
-        option.value = sizeList[key];
-        sizes.appendChild(option);
-    }
+//Populate select options for sizes
+for (var key in sizeList) {
+    var option = document.createElement("option")
+    option.text = key;
+    option.value = sizeList[key];
+    document.getElementById("packSize").appendChild(option);
 }
 
-//Update glaze premium on each change of glazing selector
+document.getElementById("glazingOptions").addEventListener('change', updateOrderPrice);
+document.getElementById("packSize").addEventListener('change', updateOrderPrice);
+
+/*//Update glaze premium on each change of glazing selector
 function glazingChange(element) {
     const priceChange = element.value
     return priceChange;
@@ -67,14 +45,14 @@ function glazingChange(element) {
 function sizeChange(element) {
     const priceChange = element.value
     return priceChange;
-}
+}*/
 
-function updatePrice(glazePremium, sizeMultiplier) {
-    document.getElementById("totalPrice").innerHTML = Math.round((((glazePremium+basePrice)*sizeMultiplier)*100)/100);
-}
+updateOrderPrice();
 
-function updateOrder() {
-    glazeType = document.getElementById("glazingOptions").value;
-    packSize = document.getElementById("packSize").value;
-    updatePrice(glazeType, packSize);
+function updateOrderPrice() {
+    glazePremium = parseFloat(document.getElementById("glazingOptions").value);
+    packMultiplier = parseFloat(document.getElementById("packSize").value);
+    price = (basePrice + glazePremium)*packMultiplier;
+   
+    document.getElementById("totalPrice").innerHTML = price.toFixed(2);
 }
